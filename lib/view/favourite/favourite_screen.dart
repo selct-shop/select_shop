0d09ada0c8327575d842be08984ad1b7334b5546
leader <leader@specialline.ae>
2024-04-14 14:26:20 +0400

    adding fav screen

---
+++ lib/view/favourite/favourite_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:select_shop/core/Shared/loading_screen.dart';
+import 'package:select_shop/core/Shared/under_develop_screen.dart';
+
+class FavoureitesScreen extends StatefulWidget {
+  const FavoureitesScreen({super.key});
+
+  @override
+  State<FavoureitesScreen> createState() => _FavoureitesScreenState();
+}
+
+class _FavoureitesScreenState extends State<FavoureitesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: UnderDevScreen(),
+    );
+  }
+}
