f54e35dd63405da2686b865b3f790bb91102a35a
leader <leader@specialline.ae>
2024-04-14 14:26:31 +0400

    adding cart screen

---
+++ lib/view/cart/cart_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:select_shop/core/Shared/under_develop_screen.dart';
+
+class CartScreen extends StatefulWidget {
+  const CartScreen({super.key});
+
+  @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const UnderDevScreen();
+  }
+}
