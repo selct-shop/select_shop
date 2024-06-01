c043401b51c7417bec7ea8df392c1bb106cffbb4
leader <leader@specialline.ae>
2024-06-01 13:21:49 +0400

    adding the discount collection ui, logic, and model

--- lib/view/home/widgets/products discounts/bloc/products_discounts_bloc.dart
+++ lib/view/home/widgets/products discounts/bloc/products_discounts_bloc.dart
@@ -10,71 +10,85 @@ import 'package:select_shop/view/home/widgets/prodcts%20best%20seller/bloc/produ
 part 'products_discounts_event.dart';
 part 'products_discounts_state.dart';
 
-class ProductsDiscountsBloc extends Bloc<ProductsDiscountsEvent, ProductsDiscountsState> {
-
-
+class ProductsDiscountsBloc
+    extends Bloc<ProductsDiscountsEvent, ProductsDiscountsState> {
   // bool loadingProductsDiscounts = true;
 
   // List<ProductCollection> newCollctionProductsList = [];
 
-
-
-  ProductsDiscountsBloc() : super(ProductsDiscountInitalState())  {
-    on<ProductsDiscountsEvent>((event, emit) async  {
+  ProductsDiscountsBloc() : super(ProductsDiscountInitalState()) {
+    on<ProductsDiscountsEvent>((event, emit) async {
       // TODO: implement event handler
 
-
-
-      
 // #### get the disscounts products #### //
-        if (event is ProductDiscountsInitalEven) {
-          emit(ProductsDiscountLoadingState());
-          // loadingNewProducts = true;
-          // get all the categories
-          try {
-            Response getDiscountProducts =
-                await DioHelper.getNewProductsHome(collection: "descounted");
-            if (getDiscountProducts.statusCode != null) {
-              if (getDiscountProducts.statusCode!.isSuccessfulHttpStatusCode) {
-                // print("reeeeeeeeeeeeeeeeeeeeeeeeeeeeeepooo");
-                // print("${getDiscountProducts.data}");
-                TheCollectionModel theCollectionModel =
-                    TheCollectionModel.fromJson(getDiscountProducts.data);
-                // newCollctionProductsList = getCollectionModel.result.products;
-                if (theCollectionModel.result.products.isEmpty) {
-                  // categoresListForHomeScreen = mainCategoResutList;
-                  // loadingNewProducts = false;
-                  emit(ProductsDiscountEmptyState(
-                    newProductCollectionList:
-                        theCollectionModel.result.products,
-                  ));
-                }
+      if (event is ProductDiscountsInitalEven) {
+        // print(
+        // "reeeeeeeeeeeeeeeeeeeeeeeeeeeeeepooo========================initalll");
 
-                if (theCollectionModel.result.products.isNotEmpty) {
-                  // categoresListForHomeScreen = mainCategoResutList;
-                  // loadingNewProducts = false;
-                  emit(ProductsDiscountSucsessState(
-                    newProductCollectionList:
-                        theCollectionModel.result.products,
-                  ));
-                }
-                // else {
-                //   emit(HomeGetHomeCatiegorErrorState(
-                //       errorMessage:
-                //           "${getHomeMainCateResponse.statusCode} \n ${getHomeMainCateResponse.statusMessage}"));
-                // }
-                // emit(HomeGetHomeCatiegorsucseesState());
-              } else {
-                emit(ProductDiscountErrorState(
-                    errorMessage: getDiscountProducts.statusMessage ??
-                        "unknown Error"));
+        emit(ProductsDiscountLoadingState());
+        // loadingNewProducts = true;
+        // get all the categories
+        try {
+          Response getDiscountProducts =
+              await DioHelper.getNewProductsHome(collection: "descounted");
+          // print(
+          //     "reeeeeeeeeeeeeeeeeeeeeeeeeeeeeepooo========================discounted");
+
+          if (getDiscountProducts.statusCode != null) {
+            // print(
+            //     "reeeeeeeeeeeeeeeeeeeeeeeeeeeeeepooo========================discounted${getDiscountProducts.statusCode}");
+            if (getDiscountProducts.statusCode!.isSuccessfulHttpStatusCode) {
+              print(
+                  "reeeeeeeeeeeeeeeeeeeeeeeeeeeeeepooo========================discounted${getDiscountProducts.statusCode}");
+              // print("reeeeeeeeeeeeeeeeeeeeeeeeeeeeeepooo");
+              // print("${getDiscountProducts.data}");
+              TheCollectionModel theCollectionModel =
+                  TheCollectionModel.fromJson(getDiscountProducts.data);
+              // newCollctionProductsList = getCollectionModel.result.products;
+
+              // print(
+              //     "reeeeeeeeeeeeeeeeeeeeeeeeeeeeeepooo========================discounted${theCollectionModel.result.products}");
+              if (theCollectionModel.result.products.isEmpty) {
+                // categoresListForHomeScreen = mainCategoResutList;
+                // loadingNewProducts = false;
+                emit(ProductsDiscountEmptyState(
+                  // newProductCollectionList: theCollectionModel.result.products,
+                  newProductCollectionList: theCollectionModel.result.products
+                ));
               }
+
+              // if (theCollectionModel.result.products.isNotEmpty) {
+              if (theCollectionModel.result.products.isNotEmpty) {
+                // print(
+                //     "reeeeeeeeeeeeeeeeeeeeeeeeeeeeeepooo========================discounted${theCollectionModel.result.products}");
+                // categoresListForHomeScreen = mainCategoResutList;
+                // loadingNewProducts = false;
+                // List<TheCollectionProduct> theProductModel =
+                //     theCollectionModel.result.products;
+                emit(ProductsDiscountSucsessState(
+                  newProductCollectionList: theCollectionModel.result.products,
+                  // newProductCollectionList: theProductModel,
+                ));
+
+                // print(
+                //     "reeeeeeeeeeeeeeeeeeeeeeeeeeeeeepooo========================discounted${theCollectionModel.result.products}");
+              }
+              // else {
+              //   emit(HomeGetHomeCatiegorErrorState(
+              //       errorMessage:
+              //           "${getHomeMainCateResponse.statusCode} \n ${getHomeMainCateResponse.statusMessage}"));
+              // }
+              // emit(HomeGetHomeCatiegorsucseesState());
+            } else {
+              emit(ProductDiscountErrorState(
+                  errorMessage:
+                      getDiscountProducts.statusMessage ?? "unknown Error"));
             }
-          } catch (exception) {
-            emit(ProductDiscountErrorState(
-                errorMessage: exception.toString()));
           }
+        } catch (exception) {
+          emit(ProductDiscountErrorState(errorMessage: exception.toString()));
         }
+      }
     });
   }
 }
--- lib/view/home/widgets/products discounts/bloc/products_discounts_state.dart
+++ lib/view/home/widgets/products discounts/bloc/products_discounts_state.dart
@@ -2,25 +2,50 @@ part of 'products_discounts_bloc.dart';
 
 sealed class ProductsDiscountsState extends Equatable {
   const ProductsDiscountsState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 final class ProductsDiscountInitalState extends ProductsDiscountsState {}
 
-
-
-  
 final class ProductsDiscountLoadingState extends ProductsDiscountsState {}
+
+///
+///
+///
+///
+///
+// final class ProductsDiscountSucsessState extends ProductsDiscountsState {
+//   final List<TheProductModel> newProductCollectionList;
+//   ProductsDiscountSucsessState({required this.newProductCollectionList});
+// }
+
+// final class ProductsDiscountEmptyState extends ProductsDiscountsState {
+//   final List<TheProductModel> newProductCollectionList;
+//   ProductsDiscountEmptyState({required this.newProductCollectionList});
+// }
+
+///
+///
+///
+///
+///
+
 final class ProductsDiscountSucsessState extends ProductsDiscountsState {
-  final List<TheProductModel> newProductCollectionList;
+  final List<TheCollectionProduct> newProductCollectionList;
   ProductsDiscountSucsessState({ required this.newProductCollectionList});
 }
 final class ProductsDiscountEmptyState extends ProductsDiscountsState {
-  final List<TheProductModel> newProductCollectionList;
-  ProductsDiscountEmptyState({ required this.newProductCollectionList}); 
+  final List<TheCollectionProduct> newProductCollectionList;
+  ProductsDiscountEmptyState({ required this.newProductCollectionList});
 }
+
+///
+///
+///
+///
+///
 final class ProductDiscountErrorState extends ProductsDiscountsState {
   final String? errorMessage;
   ProductDiscountErrorState({required this.errorMessage});
