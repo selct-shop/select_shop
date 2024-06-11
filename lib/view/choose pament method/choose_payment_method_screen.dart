840c5c535523023b6841c41bfd2552b1567d0148
leader <leader@specialline.ae>
2024-06-11 21:56:04 +0400

    complete chose payment method screen

--- lib/view/choose pament method/choose_payment_method_screen.dart
+++ lib/view/choose pament method/choose_payment_method_screen.dart
@@ -3,6 +3,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dash/flutter_dash.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:select_shop/core/constants/app_images.dart';
 // import 'package:select_shop/core/functions/nav_func.dart';
 import 'package:select_shop/core/theme/colors.dart';
 import 'package:select_shop/view/check%20out/bloc/check_out_bloc.dart';
@@ -24,7 +26,7 @@ List<Map<String, dynamic>> paymentTypes = [
 
 // Map<String, dynamic> activePaymentType = paymentTypes[0];
 // int activePaymentType = 1;
-Map<String, dynamic> activePaymentType = paymentTypes[0];
+Map<String, dynamic> activePaymentType = paymentTypes[1];
 
 class ChoosePaymentMethodScreen extends StatefulWidget {
   const ChoosePaymentMethodScreen({super.key});
@@ -64,76 +66,101 @@ class _ChoosePaymentMethodScreenState extends State<ChoosePaymentMethodScreen> {
                     // navigateTo(context, CreateNewLocationScreen());
                   },
                   child: Container(
-                    height: 45,
+                    height: 250,
                     width: double.infinity,
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(10),
                       boxShadow: [
                         BoxShadow(
-                          offset: Offset(0, 2),
-                          blurRadius: 2,
-                          spreadRadius: 2,
+                          offset: Offset(0, 1),
+                          blurRadius: 1,
+                          spreadRadius: 1,
                           color: Colors.black12,
                         ),
                       ],
                     ),
-                    child: Row(
+                    child: Column(
                       children: [
+                        Row(
+                          children: [
+                            const SizedBox(
+                              width: 15,
+                            ),
+                            // cach svg image
+                            const Icon(
+                              Icons.add_circle_outline_rounded,
+                              size: 24,
+                              color: AppColors.mainColor,
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              "pay on delivery",
+                              style: TextStyle(
+                                color: AppColors.grey2Color,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                height: .8,
+
+                                // overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            const Spacer(),
+
+                            // here cash payment
+                            Radio<Map<String, dynamic>>(
+                                key: cardRadioButtonKey,
+                                // value: paymentTypes[0]["value"],
+                                value: paymentTypes[0],
+                                groupValue: activePaymentType,
+                                // activeColor: AppColors.mainColor,
+
+                                fillColor:
+                                    MaterialStateProperty.resolveWith((states) {
+                                  // active
+                                  if (states.contains(MaterialState.selected)) {
+                                    return AppColors.mainColor;
+                                  }
+                                  // inactive
+                                  return AppColors.grey2Color;
+                                }),
+                                // fillColor:
+                                //     MaterialStatePropertyAll(AppColors.grey2Color),
+                                onChanged: (value) {
+                                  // activePaymentType = paymentTypes[value][0];
+                                  activePaymentType = value!;
+                                  context.read<CheckOutBloc>().paymentMethod =
+                                      111;
+
+                                  // print("$value valllllllllllllllllllllllllll");
+                                  // i should use the bloc instade of set state
+                                  setState(() {});
+                                }),
+                            const SizedBox(
+                              width: 15,
+                            ),
+                          ],
+                        ),
                         const SizedBox(
+                          height: 15,
                           width: 15,
                         ),
-                        // cach svg image
-                        const Icon(
-                          Icons.add_circle_outline_rounded,
-                          size: 24,
-                          color: AppColors.mainColor,
-                        ),
-                        const SizedBox(
-                          width: 20,
-                        ),
-                        Text(
-                          "pay on delivery",
-                          style: TextStyle(
-                            color: AppColors.grey2Color,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                            height: .8,
-
-                            // overflow: TextOverflow.ellipsis,
+                        Expanded(
+                            child: SizedBox(
+                          height: double.infinity,
+                          width: double.infinity,
+                          child: SvgPicture.asset(
+                            AppImagesSvg.cashSvg,
+                            color: AppColors.mainColor,
+                            // height: double.infinity,
+                            // width: double.infinity,
+                            fit: BoxFit.fitHeight,
                           ),
-                        ),
-                        const Spacer(),
-
-                        // here cash payment
-                        Radio<Map<String, dynamic>>(
-                            key: cardRadioButtonKey,
-                            // value: paymentTypes[0]["value"],
-                            value: paymentTypes[0],
-                            groupValue: activePaymentType,
-                            // activeColor: AppColors.mainColor,
-
-                            fillColor:
-                                MaterialStateProperty.resolveWith((states) {
-                              // active
-                              if (states.contains(MaterialState.selected)) {
-                                return AppColors.mainColor;
-                              }
-                              // inactive
-                              return AppColors.grey2Color;
-                            }),
-                            // fillColor:
-                            //     MaterialStatePropertyAll(AppColors.grey2Color),
-                            onChanged: (value) {
-                              // activePaymentType = paymentTypes[value][0];
-                              activePaymentType = value!;
-                              context.read<CheckOutBloc>().paymentMethod = 111;
-
-                              // print("$value valllllllllllllllllllllllllll");
-                              // i should use the bloc instade of set state
-                              setState(() {});
-                            }),
+                        )),
                         const SizedBox(
+                          height: 15,
                           width: 15,
                         ),
                       ],
@@ -160,78 +187,101 @@ class _ChoosePaymentMethodScreenState extends State<ChoosePaymentMethodScreen> {
                     // cardRadioButtonKey.
                   },
                   child: Container(
-                    height: 45,
+                    height: 250,
                     width: double.infinity,
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(10),
                       boxShadow: [
                         BoxShadow(
-                          offset: Offset(0, 2),
-                          blurRadius: 2,
-                          spreadRadius: 2,
+                          offset: Offset(0, 1),
+                          blurRadius: 1,
+                          spreadRadius: 1,
                           color: Colors.black12,
                         ),
                       ],
                     ),
-                    child: Row(
+                    child: Column(
                       children: [
+                        Row(
+                          children: [
+                            const SizedBox(
+                              width: 15,
+                            ),
+                            // cach svg image
+                            const Icon(
+                              Icons.add_circle_outline_rounded,
+                              size: 24,
+                              color: AppColors.mainColor,
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              "credit card payment",
+                              style: TextStyle(
+                                color: AppColors.grey2Color,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                height: .8,
+
+                                // overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            const Spacer(),
+
+                            // here pay by card option
+                            Radio<Map<String, dynamic>>(
+                                key: cardRadioButtonKey,
+                                value: paymentTypes[1],
+                                groupValue: activePaymentType,
+                                // activeColor: AppColors.mainColor,
+                                // fillColor:
+                                //     MaterialStatePropertyAll(AppColors.grey2Color),
+
+                                fillColor:
+                                    MaterialStateProperty.resolveWith((states) {
+                                  // active
+                                  if (states.contains(MaterialState.selected)) {
+                                    return AppColors.mainColor;
+                                  }
+                                  // inactive
+                                  return AppColors.grey2Color;
+                                }),
+                                onChanged: (value) {
+                                  activePaymentType = value!;
+
+                                  // i should use the bloc instade of set state
+                                  context.read<CheckOutBloc>().paymentMethod =
+                                      222;
+                                  // print(
+                                  //     "$value valllllllllllllllllllllllllll${context.read<CheckOutBloc>().paymentMethod}");
+
+                                  // print(
+                                  //     "val99999999999999999999llllllllllllllllllllllllll");
+                                  setState(() {});
+                                }),
+                            const SizedBox(
+                              width: 15,
+                            ),
+                          ],
+                        ),
                         const SizedBox(
+                          height: 15,
                           width: 15,
                         ),
-                        // cach svg image
-                        const Icon(
-                          Icons.add_circle_outline_rounded,
-                          size: 24,
-                          color: AppColors.mainColor,
-                        ),
-                        const SizedBox(
-                          width: 20,
-                        ),
-                        Text(
-                          "credit card payment",
-                          style: TextStyle(
-                            color: AppColors.grey2Color,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                            height: .8,
-
-                            // overflow: TextOverflow.ellipsis,
+                        Expanded(
+                            child: SizedBox(
+                          height: double.infinity,
+                          width: double.infinity,
+                          child: SvgPicture.asset(
+                            AppImagesSvg.creditCardsSvg,
+                            color: AppColors.mainColor,
+                            fit: BoxFit.fitHeight,
                           ),
-                        ),
-                        const Spacer(),
-
-                        // here pay by card option
-                        Radio<Map<String, dynamic>>(
-                            key: cardRadioButtonKey,
-                            value: paymentTypes[1],
-                            groupValue: activePaymentType,
-                            // activeColor: AppColors.mainColor,
-                            // fillColor:
-                            //     MaterialStatePropertyAll(AppColors.grey2Color),
-
-                            fillColor:
-                                MaterialStateProperty.resolveWith((states) {
-                              // active
-                              if (states.contains(MaterialState.selected)) {
-                                return AppColors.mainColor;
-                              }
-                              // inactive
-                              return AppColors.grey2Color;
-                            }),
-                            onChanged: (value) {
-                              activePaymentType = value!;
-
-                              // i should use the bloc instade of set state
-                              context.read<CheckOutBloc>().paymentMethod = 222;
-                              // print(
-                              //     "$value valllllllllllllllllllllllllll${context.read<CheckOutBloc>().paymentMethod}");
-
-                              // print(
-                              //     "val99999999999999999999llllllllllllllllllllllllll");
-                              setState(() {});
-                            }),
+                        )),
                         const SizedBox(
+                          height: 15,
                           width: 15,
                         ),
                       ],
@@ -245,84 +295,6 @@ class _ChoosePaymentMethodScreenState extends State<ChoosePaymentMethodScreen> {
                 ///
                 ///
                 ///
-                const SizedBox(
-                  height: 15,
-                  width: 15,
-                ),
-                Container(
-                  // user cards List
-                  height: 250,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 2),
-                        blurRadius: 2,
-                        spreadRadius: 2,
-                        color: Colors.black12,
-                      ),
-                    ],
-                  ),
-                  child: Row(
-                    children: [
-                      const SizedBox(
-                        width: 15,
-                      ),
-                      // cach svg image
-                      const Icon(
-                        Icons.add_circle_outline_rounded,
-                        size: 24,
-                        color: AppColors.mainColor,
-                      ),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                      Text(
-                        "credit card payment",
-                        style: TextStyle(
-                          color: AppColors.grey2Color,
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          height: .8,
-
-                          // overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                      const Spacer(),
-
-                      // here pay by card option
-                      Radio<Map<String, dynamic>>(
-                          key: cardRadioButtonKey,
-                          value: paymentTypes[1],
-                          groupValue: activePaymentType,
-                          // activeColor: AppColors.mainColor,
-                          // fillColor:
-                          //     MaterialStatePropertyAll(AppColors.grey2Color),
-
-                          fillColor:
-                              MaterialStateProperty.resolveWith((states) {
-                            // active
-                            if (states.contains(MaterialState.selected)) {
-                              return AppColors.mainColor;
-                            }
-                            // inactive
-                            return AppColors.grey2Color;
-                          }),
-                          onChanged: (value) {
-                            activePaymentType = value!;
-
-                            print("$value valllllllllllllllllllllllllll");
-                            // i should use the bloc instade of set state
-                            setState(() {});
-                          }),
-                      const SizedBox(
-                        width: 15,
-                      ),
-                    ],
-                  ),
-                ),
 
                 ///
                 ///
@@ -351,9 +323,9 @@ class _ChoosePaymentMethodScreenState extends State<ChoosePaymentMethodScreen> {
                       borderRadius: BorderRadius.circular(10),
                       boxShadow: [
                         BoxShadow(
-                          offset: Offset(0, 2),
-                          blurRadius: 2,
-                          spreadRadius: 2,
+                          offset: Offset(0, 1),
+                          blurRadius: 1,
+                          spreadRadius: 1,
                           color: Colors.black12,
                         ),
                       ],
